c9d9554920720e7e4e4b721d765abc02fa826205
Robbe Dekien <robbe.dekien@student.hogent.be>
2018-09-28 10:06:24 +0200

    first commit

---
+++ Banking/Models/Domein/BankAccount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.Models.Domein
+{
+    class BankAccount
+    {
+        private string _accountNumber;
+        private decimal _balance;
+
+        public string AccountNumber { get; private set; }
+
+        //public decimal GetBalance()
+        //{
+        //    return _balance;
+        //}
+
+        //public void SetBalance(decimal value)
+        //{
+        //    _balance = value;
+        //}
+
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException("No negative balance allowed");
+                _balance = value;
+            }
+        }
+        #region Constructors
+        public BankAccount(string accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber)
+        {
+            Balance = balance;
+        }
+        #endregion
+
+        #region Methodes
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        #endregion
+
+    }
+}
+
+
---
+++ Banking/Program.cs
@@ -0,0 +1,20 @@
+﻿using Banking.Models.Domein;
+using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myBA = new BankAccount("123-12312312-999" , 50);
+            Console.WriteLine($"Accountnummer is  {myBA.AccountNumber}");
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            myBA.Deposit(200);
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            myBA.Withdraw(50);
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            Console.ReadKey();
+        }
+    }
+}
